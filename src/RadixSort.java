fedf2d8668fddac770e8bbbcb9cf0c4e860a5326
shilpa <shilpadolai@gmail.com>
2024-05-01 19:15:12 +0530

    radix sort

---
+++ src/RadixSort.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+class RadixSort {
+
+    // Using counting sort to sort the elements in the basis of significant places
+    void countingSort(int array[], int size, int place) {
+        int[] output = new int[size + 1];
+
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        // Calculate count of elements
+        for (int i = 0; i < size; i++)
+            count[(array[i] / place) % 10]++;
+
+        // Calculate cumulative count
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        // Place the elements in sorted order
+        // If you are curious the code for counting sort is obviously the same except
+        // for output[array[i]] is now count[(array[i] / place) % 10]
+        // The reason being we are now trying to map the digits , not the entire number
+        for (int i = size - 1; i >= 0; i--) {
+            output[count[(array[i] / place) % 10] - 1] = array[i];
+            count[(array[i] / place) % 10]--;
+        }
+
+        for (int i = 0; i < size; i++)
+            array[i] = output[i];
+    }
+
+    int getMax(int array[], int n) {
+        int max = array[0];
+        for (int i = 1; i < n; i++)
+            if (array[i] > max)
+                max = array[i];
+        return max;
+    }
+
+    void radixSort(int array[], int size) {
+
+        int max = getMax(array, size);
+
+        // Apply counting sort to sort elements based on place value.
+        for (int place = 1; max / place > 0; place *= 10)
+            countingSort(array, size, place);
+    }
+
+    public static void main(String args[]) {
+        int[] data = { 121, 432, 564, 23, 1, 45, 788 };
+        int size = data.length;
+        RadixSort rs = new RadixSort();
+        rs.radixSort(data, size);
+        System.out.println("Sorted Array in Ascending Order: ");
+        System.out.println(Arrays.toString(data));
+    }
+}
