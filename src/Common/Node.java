da19aebf96fa9af8bbe4d2bd6ec53c6c3fa7a864
shilpa <shilpadolai@gmail.com>
2024-05-03 17:51:31 +0530

    Gneric Node class

---
+++ src/Common/Node.java
@@ -0,0 +1,11 @@
+package Common;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key, Node next) {
+        this.key = key;
+        this.next = next;
+    }
+}
